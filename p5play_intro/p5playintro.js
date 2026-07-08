ef809632f2c9f321c3a4e77c0a8cbe2e0d7404cf
Walden-pro <waldenchho@gmail.com>
2026-07-08 08:54:40 +0000

    Jul 8, 2026, 8:54 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -17,7 +17,7 @@ function setup() {
   // fill("#b8330ea8");
   // triangle(50,50,400,200,50,350)
 
-  quad(400,50, 700,350, 400,100, 100,350)
+  // quad(400,50, 700,350, 400,100, 100,350)
   // End Basic shape testing
 
    // Create a bouncing ball sprite
