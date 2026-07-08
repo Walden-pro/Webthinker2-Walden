312cbb64564d782d45f12b0cc76b179b8296ec95
Walden-pro <waldenchho@gmail.com>
2026-07-08 08:36:44 +0000

    Jul 8, 2026, 8:36 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -2,12 +2,13 @@ function setup() {
   // Set up the canvas
   new Canvas(800, 400);
   background(250); //background color
-  fill("#0eb89ba8")
+
   // ball = new Sprite(50,50,50,50);
 
   // Basic shape testing
   // write your codes here
- 
+  fill("#0eb89ba8")
+  
   // End Basic shape testing
 
    // Create a bouncing ball sprite
