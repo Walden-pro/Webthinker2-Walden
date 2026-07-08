8c2b87b914109cc6118eb36fb7dcc6d1192b1e90
Walden-pro <waldenchho@gmail.com>
2026-07-08 07:57:08 +0000

    Jul 8, 2026, 7:57 AM

--- Every single thingy/Lesson 1/L01_redCircle.js
+++ Every single thingy/Lesson 1/L01_redCircle.js
@@ -4,15 +4,16 @@ function setup(){
     let x = 5;
     let y = 10;
     let total = x + y;
+    conc
 }
 
 function draw(){
-    fill(255, 0, 0);
-    ellipse(300,200,50,50);
-    fill(216, 219, 0);
-    ellipse(300,250,50,50);
-    fill(44, 219, 76);
-    ellipse(300,300,50,50);
+    // fill(255, 0, 0);
+    // ellipse(300,200,50,50);
+    // fill(216, 219, 0);
+    // ellipse(300,250,50,50);
+    // fill(44, 219, 76);
+    // ellipse(300,300,50,50);
 }
 
 
