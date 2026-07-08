386f893ca2d10f6341f9a0e52d63570ab39ac42b
Walden-pro <waldenchho@gmail.com>
2026-07-08 08:43:47 +0000

    Jul 8, 2026, 8:43 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -7,12 +7,12 @@ function setup() {
 
   // Basic shape testing
   // write your codes here
-  fill("#0eb89ba8")
-  stroke("#000000a8")
-  strokeWeight(10)
+  fill("#0eb89ba8");
+  stroke("#000000a8");
+  strokeWeight(10);
   
-  circle(400,200,100)
-  Rect(600, 300,100,100)
+  circle(400,200,100);
+  Rect(600, 300,100,100);
   // End Basic shape testing
 
    // Create a bouncing ball sprite
