7e24c386b1bbef4df5fb152400fe369a1fdb46c0
Walden-pro <waldenchho@gmail.com>
2026-07-08 08:32:49 +0000

    Jul 8, 2026, 8:32 AM

--- p5play_intro/p5playintro.js
+++ p5play_intro/p5playintro.js
@@ -3,7 +3,8 @@ function setup() {
   new Canvas(800, 400);
   background(250); //background color
   ball = new Sprite();
-  ball.x
+  ball.x = 0
+  ball.y = 0
   // Basic shape testing
   // write your codes here
  
