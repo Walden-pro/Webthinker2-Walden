9ef1735e9c4b6ab771096b760f93cf9607fb65ed
Walden-pro <waldenchho@gmail.com>
2026-07-15 08:21:12 +0000

    Jul 15, 2026, 8:21 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -63,7 +63,7 @@ function setup() {
 
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here
-  let number = 19;
+  let number = 21;
   while (number != 1) {
     number -= 2;
     console.log(number);
