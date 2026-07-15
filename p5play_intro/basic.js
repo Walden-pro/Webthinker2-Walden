40e069a46fdae69e18236333e16bacf67c9b3f7e
Walden-pro <waldenchho@gmail.com>
2026-07-15 08:35:38 +0000

    Jul 15, 2026, 8:35 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -79,7 +79,7 @@ function setup() {
   console.log(groceries[1]);
   // groceries.shift()
   // groceries.pop()
-  groceries.splice(0,1, "kaya")
+  groceries.splice(0,3, "kaya", "balls", "")
   console.log(groceries);
 
 
