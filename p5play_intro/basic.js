aceb23ad653efe502cc2e499e6c878432b4b5687
Walden-pro <waldenchho@gmail.com>
2026-07-15 07:48:51 +0000

    Jul 15, 2026, 7:48 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -16,7 +16,7 @@ function setup() {
   // write your codes here
   let base = 10;
   let height = 5;
-  let area = 0.5 * base * base;
+  let area = 0.5 * base * height;
   console.log("Area: " + area)
   textSize(4)
   text(area)
