6f1c0719ee53d8cb3700b34064c8989b5c7925b3
Walden-pro <waldenchho@gmail.com>
2026-07-15 07:41:41 +0000

    Jul 15, 2026, 7:41 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -10,7 +10,8 @@ function setup() {
 
 function setup() {
   // write your codes here
-
+  new Canvas(800, 400);
+  background(250); //background color
   // --- Exercise: Area of Triangle ---
   // write your codes here
 
