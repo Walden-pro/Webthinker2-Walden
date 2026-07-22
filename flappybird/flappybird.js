050a8812db022233a7fec05437cd8dd48283bd9b
Walden-pro <waldenchho@gmail.com>
2026-07-22 08:57:21 +0000

    Jul 22, 2026, 8:57 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -50,6 +50,7 @@ function draw() {
   }
 
   text("vel.y " + bird.vel.y.toFixed(2),10,20); // (text,x,y)
-  text("isMoving:" + bird.isMoving,10,40)
+  text("isMoving:" + bird.isMoving,10,40);
+  text("isMoving:" + bird.isMoving,10,40);
 
 }
