abca5c5dbb61d5425432593f72e40825313641d6
Walden-pro <waldenchho@gmail.com>
2026-07-29 09:01:22 +0000

    Jul 29, 2026, 9:01 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -43,7 +43,7 @@ function setup() {
   bird.mass = 2;//heavyness
   bird.bounciness = 0.5;//higher is bouncier
   bird.drag = 0.02;//air resistance
-  bird.collider = "dynamic";//collidable, movable and affected by physics
+  bird.collider = "static";//collidable, movable and affected by physics
 
 //make floor
   floor = new Sprite();
@@ -58,7 +58,7 @@ function setup() {
   startScreenLabel = new Sprite(width/2, height/2, 50, 50,'none');
   startScreenLabel.img = startScreenImg;
   bird.visible = false;
-  bird.y = 175
+
 }
 function draw() {
   image(background,0,0,width,height);
