efeb970a1cdc94b6c0f55a80bf7186f81ef1564d
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:31:54 +0000

    Jul 29, 2026, 8:31 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -16,7 +16,7 @@ base = loadImage("assets/base.png");
 birdDownImg = loadImage("assets/yellowbird-downflap.png");
 birdUpImg = loadImage("assets/yellowbird-upflap.png");
 pipe = loadImage("assets/pipe-green.png");
-
+game
 }
 function setup() {
   
