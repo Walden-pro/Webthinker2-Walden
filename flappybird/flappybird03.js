dc1977911157888b88a1320fa14bb34335c56f8d
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:54:47 +0000

    Jul 29, 2026, 8:54 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -58,7 +58,7 @@ function setup() {
   startScreenLabel = new Sprite(width/2, height/2, 50, 50,'none');
   startScreenLabel.img = startScreenImg;
   bird.visible = false;
-  bird.y = 150
+  bird.y = 200
 }
 function draw() {
   image(background,0,0,width,height);
