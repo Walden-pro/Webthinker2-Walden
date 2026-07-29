bd1c8882d6dfe26747b9f326e3ebed23db9efdaf
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:05:32 +0000

    Jul 29, 2026, 8:05 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -87,7 +87,7 @@ bottomPipe.img = pipe;
 pipeGroup.add(bottomPipe);
 pipeGroup.layer = 0;
 
-topPipe = new Sprite (400, midY - gap)
+topPipe = new Sprite (400, midY - gap / 2 - 200,52,320, ' static')
 }
 
 
