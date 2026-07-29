4df5f27438562cd33378a6d1524ab3d69d8f0072
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:02:44 +0000

    Jul 29, 2026, 8:02 AM

--- flappybird/flappybird02.js
+++ flappybird/flappybird02.js
@@ -80,7 +80,8 @@ function spawnPipePair(){
 let gap = 50;
 let midY = height / 2;
 
-bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52,)
+bottomPipe = new Sprite(400, midY + gap / 2 + 200, 52,320,'static')
+
 
 }
 
