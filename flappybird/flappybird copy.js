ae45fa3eb681460b52b22509933864bdf5ad7461
Walden-pro <waldenchho@gmail.com>
2026-07-29 07:33:53 +0000

    Jul 29, 2026, 7:33 AM

---
+++ flappybird/flappybird copy.js
@@ -0,0 +1,56 @@
+let bird;
+let floor;
+let birdMidImg;
+let background;
+let base;
+
+function preload(){
+birdMidImg = loadImage("assets/yellowbird-midflap.png");
+background = loadImage("assets/background-day.png");
+base = loadImage("assets/base.png");
+
+}
+function setup() {
+  
+  new Canvas(400, 600);
+
+//gravity
+  world.gravity.y = 10;
+
+// debug text
+  fill("#fc0303")
+  textSize(14)
+
+// make bird
+  bird = new Sprite();
+  bird.img = birdMidImg;
+  bird.width = 30;
+  bird.height = 30;
+  bird.mass = 2;//heavyness
+  bird.bounciness = 0.5;//higher is bouncier
+  bird.drag = 0.02;//air resistance
+  bird.collider = "dynamic";//collidable, movable and affected by physics
+
+//make floor
+  floor = new Sprite();
+  floor.img = base;
+  floor.width = width;
+  floor.height = 125;
+  floor.x = width/2;
+  floor.y = height - 20;
+  floor.collider = "static";//collidable but will not move
+
+
+}
+function draw() {
+  image(background,0,0,width,height);
+  if(kb.presses ("space") || mouse.presses("left")){
+    bird.sleeping = false;// make sure no sprite is not idle
+    bird.vel.y = -5; //Up velocity
+  }
+
+  text("vel.y " + bird.vel.y.toFixed(2),10,20); // (text,x,y)
+  text("isMoving:" + bird.isMoving,10,40);
+  text("Sleeping:" + bird.isMoving,10,60);
+
+}
