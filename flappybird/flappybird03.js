34c0de62dcf7a9670e9ca4afdb1df38f5e4e0a21
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:37:45 +0000

    Jul 29, 2026, 8:37 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -91,10 +91,10 @@ function draw() {
     }
   }
   if (bird.collides(pipeGroup)|| bird.collides(floor)){
-    
     gameOverLabel = new Sprite(width/2,height / 2, 192,42,'none');
     gameOverLabel.img = gameOverImg;
     gameOverLabel = 
+    noLoop();
   }
   
 
