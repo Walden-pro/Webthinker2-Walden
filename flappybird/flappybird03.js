5f7d0943c107bb12ee229576039596e99ef2360a
Walden-pro <waldenchho@gmail.com>
2026-07-29 08:20:16 +0000

    Jul 29, 2026, 8:20 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -80,7 +80,7 @@ function draw() {
   camera.x = bird.x;//lock the camera to the bird postion
   floor.x = bird.x;//lock the bird in the position
 
-  if (frameCount % 90 === 0){//change the pipe to spawn slower. 60 frames 
+  if (frameCount % 90 === 0){//change the pipe to spawn slower. 60 frames is one second.
     spawnPipePair();
   }
   for (let pipe of pipeGroup){
