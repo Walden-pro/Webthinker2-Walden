ca11351536b6d1021ce9d29575b18247dccf9d4d
Walden-pro <waldenchho@gmail.com>
2026-08-08 06:21:48 +0000

    Aug 8, 2026, 6:21 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -63,6 +63,7 @@ function setup() {
   startScreenLabel = new Sprite(width/2, height/2, 50, 50,'none');
   startScreenLabel.img = startScreenImg;
   bird.visible = false;
+  
   scoreDigits = new Group();
   scoreDigits.collider = 'none';
   scoreDigits.layer = 1000;
