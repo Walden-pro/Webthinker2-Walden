6bfc367e3757ef175f994aa923743f30fb53e193
Walden-pro <waldenchho@gmail.com>
2026-08-08 06:36:36 +0000

    Aug 8, 2026, 6:36 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -155,7 +155,8 @@ function drawScore(x,y,score,digitWit=dth, digitHeight){
   for(let i = 0;i<scoreStr.length;i++){
     let digit = int(scoreStr[i]);
     let xPos = startX +i * digitWidth;
-    let digitSprite = new scoreDigits.Sprite(xPos,y,)
+    let digitSprite = new scoreDigits.Sprite(xPos,y,digitWidth,digitHeight);
+    
   }
 }
 
