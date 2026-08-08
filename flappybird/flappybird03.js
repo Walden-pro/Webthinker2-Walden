04321b1b13bb3082a05639334e5f0ba8ff465c68
Walden-pro <waldenchho@gmail.com>
2026-08-08 06:43:26 +0000

    Aug 8, 2026, 6:43 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -163,8 +163,8 @@ function drawScore(x,y,score,digitWidth, digitHeight){
 function moveGroup(group,tagetX,spacing){
   let totalWidth = (group.length -1) * spacing;
   let startX = (targetX - totalWidth/2);
-  for (let i = 0;i< group.length; i++){
-    group[i].x = startX + i * spacing
+  for (let i = 0; i < group.length; i++){
+    group[i].x = startX + i * spacing;
   }
 }
 
