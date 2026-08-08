94388311e14b83c5a0dcda2b58ac927da2cb1fa4
Walden-pro <waldenchho@gmail.com>
2026-08-08 06:17:40 +0000

    Aug 8, 2026, 6:17 AM

--- flappybird/flappybird03.js
+++ flappybird/flappybird03.js
@@ -13,6 +13,7 @@ let startScreenLabel;
 let startScreenImg;
 let startGame = false;
 
+
 function preload(){
 birdMidImg = loadImage("assets/yellowbird-midflap.png");
 background = loadImage("assets/background-day.png");
