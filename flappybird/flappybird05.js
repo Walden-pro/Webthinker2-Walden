e9deed4794f9db29142e1f03cd20a1e211b1458b
Walden-pro <waldenchho@gmail.com>
2026-08-12 08:06:19 +0000

    Aug 12, 2026, 8:06 AM

--- flappybird/flappybird05.js
+++ flappybird/flappybird05.js
@@ -138,7 +138,7 @@ function draw() {
     let birdLeftEdge = bird.x - bird.w /2;
     if (pipe.passed == false && pipeRightEdge <birdLeftEdge){
       pipe.passed = true;
-      point
+      pointSound.play();
       score++;
     }
   }
