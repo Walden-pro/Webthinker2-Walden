0efb78b42d57c2d728be4fe79dd8554c84fc3a75
Walden-pro <waldenchho@gmail.com>
2026-08-19 08:23:17 +0000

    Aug 19, 2026, 8:23 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -19,8 +19,8 @@ function setup(){
     //Peach
     peach = new Sprite();
     peach.img = peachWhole;
-    peach.width = 127.5;
-    peach.height = 127.5;
+    peach.width = 135;
+    peach.height = 135;
     peach.scale = peach.width / peachWhole.width;
     peach.mass = 2;//How heavy
     peach.drag = 0.02;//Air resistance
