2a851901ea0ed007e1b518d1ba02204ec29a832e
Walden-pro <waldenchho@gmail.com>
2026-08-19 08:03:47 +0000

    Aug 19, 2026, 8:03 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -14,7 +14,13 @@ function setup(){
     new Canvas(800, 600);
     world.gravity.y = 10;
     peach = new Sprite();
-    
+    bird.img = birdMidImg;
+  bird.width = 30;
+  bird.height = 30;
+  bird.mass = 2;//heavyness
+  bird.bounciness = 0.5;//higher is bouncier
+  bird.drag = 0.02;//air resistance
+  bird.collider = "static";//collidable, movable and affected by physics
 
 }
 function draw(){
