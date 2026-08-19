86c3df668a82212e1ae189457a9b7dd1c0a901b0
Walden-pro <waldenchho@gmail.com>
2026-08-19 07:56:49 +0000

    Aug 19, 2026, 7:56 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -4,6 +4,7 @@ let peachwhole;
 function preload(){
     background = loadImage("assets/dojobackground.png");
     watermelonwhole = loadImage("assets/watermelonwhole.png")
+    wmelonwhole = loadImage("assets/watermelonwhole.png")
 }
 function setup(){
   new Canvas(800, 600);
