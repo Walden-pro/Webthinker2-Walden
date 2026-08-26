acc72294b4b24a9aa0bdb7c29f901abcd4ed4283
Walden-pro <waldenchho@gmail.com>
2026-08-26 07:49:29 +0000

    Aug 26, 2026, 7:49 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -64,4 +64,5 @@ function spawnFruit(){}
   let fruitData = random(fruitTypes);
   let randomX = random(300,500);
   let fruit = new fruitGroup.Sprite(randomX, height+20, 40);
-  fruit.image = fruitData
+  fruit.image = fruitData.whole;
+  fruit.type = fruitData
