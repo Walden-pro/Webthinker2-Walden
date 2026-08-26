1e7df395aa130e2a8448bdd04042b071b7117a68
Walden-pro <waldenchho@gmail.com>
2026-08-26 07:39:55 +0000

    Aug 26, 2026, 7:39 AM

---
+++ fruitninja/fruitninja copy.js
@@ -0,0 +1,59 @@
+
+
+//Variables
+let watermelon;
+let peach;
+
+//Preload variables
+let background;
+let watermelonWhole;
+let peachWhole;
+
+function preload(){
+    background = loadImage("assets/dojobackground.png");
+    watermelonWhole = loadImage("assets/watermelonwhole.png");
+    peachWhole = loadImage("assets/peachwhole.png");
+}
+function setup(){
+
+    new Canvas(800, 600);
+    world.gravity.y = 10;
+
+
+    //Groups
+    peachGroup = new Group();
+    watermelonGroup = new Group();
+}
+function draw(){
+    image(background,0,0,width,height);
+    fill("#ba0000");
+    textSize(50);
+    text("framecount:" + frameCount,10,40);
+
+    if (frameCount % 60 == 0){
+        spawnPeach();
+        spawnWatermelon();
+    }
+
+}
+function spawnPeach(){
+    peach = new peachGroup.Sprite();
+    peach.img = peachWhole;
+    peach.width = 150;
+    peach.height = 150;
+    peach.scale = peach.width / peachWhole.width;
+    peach.mass = 2;//How heavy
+    peach.drag = 0.02;//Air resistance
+    peach.collider = "dynamic";//Collidable,movable,affected by physics
+}
+function spawnWatermelon(){
+    watermelon = new watermelonGroup.Sprite();
+    watermelon.img = watermelonWhole;
+    watermelon.width = 150;
+    watermelon.height = 150;
+    watermelon.scale = watermelon.width / watermelonWhole.width;
+    watermelon.mass = 2;//How heavy
+    watermelon.drag = 0.02;//Air resistance
+    watermelon.collider = "dynamic";//Collidable,movable,affected by physics
+}
+function spawnFruit(){}
