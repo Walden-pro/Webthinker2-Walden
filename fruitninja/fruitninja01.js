65b69ee30f0596109c6b56fc459a25fc6ecfc4dc
Walden-pro <waldenchho@gmail.com>
2026-09-16 07:59:40 +0000

    Sep 16, 2026, 7:59 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -140,7 +140,6 @@ function sliceFruit(){
         if (fruit.sliced){
             continue;
         }
-   
         let d = dist(mouse.x, mouse.y, fruit.x , fruit.y);
         if (d< ((fruit.d / 2 )+ 5)) {
             fruit.sliced = true;
@@ -151,6 +150,7 @@ function sliceFruit(){
             break;
         }
     }
+    
 }
 function splitFruit(x,y,fruitData){
     let left = new fruitHalves.Sprite(x - 10,y,40 , 40);
