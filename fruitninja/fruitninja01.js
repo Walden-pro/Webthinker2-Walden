9c508387e52168bb1c3710c208180f3d02531043
Walden-pro <waldenchho@gmail.com>
2026-09-16 07:35:08 +0000

    Sep 16, 2026, 7:35 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -85,7 +85,7 @@ function draw(){
     fill("#ba0000");
     textSize(50);
     text("framecount:" + frameCount,10,40);
-    if (frameCount % 120 == 0){
+    if (frameCount % 1 == 0){
         spawnsFruit();
     }
     if (mouse.pressing()){
