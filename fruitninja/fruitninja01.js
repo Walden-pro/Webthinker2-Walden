93bb88714bffba16f81c04adfb449f534cc0340d
Walden-pro <waldenchho@gmail.com>
2026-09-16 07:46:58 +0000

    Sep 16, 2026, 7:46 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -83,9 +83,9 @@ function preload(){
         scaleMod:2,
     }
     let coconutold = {
-        whole : loadImage("assets/starfruit1.png"),
-        half1: loadImage("assets/starfruit2.png"),
-        half2: loadImage("assets/starfruit2.png"),
+        whole : loadImage("assets/coconutold1.png"),
+        half1: loadImage("assets/coconutold2.png"),
+        half2: loadImage("assets/coconutold2.png"),
         scaleMod:2,
     }
     //added into fruits
