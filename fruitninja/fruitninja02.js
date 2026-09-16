19e7cee60ec33a50c1788e2e256abda521f4353b
Walden-pro <waldenchho@gmail.com>
2026-09-16 08:51:08 +0000

    Sep 16, 2026, 8:51 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -6,7 +6,7 @@ let fruitHalves;
 let fruitTypes = [];
 let score = 0;
 let missed = 0;
-let gameState = 'playing';//start,playing , gameover
+let gameState = 'start';//start,playing , gameover
 
 function preload(){
     background = loadImage("assets/dojobackground.png")
