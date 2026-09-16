70aef86905a269015b6ed88fdc552834f8e05e0a
Walden-pro <waldenchho@gmail.com>
2026-09-16 08:26:28 +0000

    Sep 16, 2026, 8:26 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -111,7 +111,7 @@ function draw(){
     image(background,0,0,width,height);
     if (gameState === "start") {
 
-    }else if ()
+    }else if (gamestate === "playing")
     fill("rgba(60, 240, 24, 0.99)"); // a for alpha means transparency
     textSize(30);
     // text("framecount:" + frameCount,10,10);
