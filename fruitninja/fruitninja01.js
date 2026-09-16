f0ae5047613c3ad06ea63742a5b2a3dcd3e8a0ff
Walden-pro <waldenchho@gmail.com>
2026-09-16 08:29:14 +0000

    Sep 16, 2026, 8:29 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -113,24 +113,24 @@ function draw(){
         return;
     }else if (gamestate === "playing"){
         fill("rgba(60, 240, 24, 0.99)"); // a for alpha means transparency
-    textSize(30);
-    // text("framecount:" + frameCount,10,10);
-    text("Score:" + score,10,40);
-    fill("rgba(209, 221, 206, 0.99)");
-    text("Missed:" + missed,10,100);
+        textSize(30);
+        // text("framecount:" + frameCount,10,10);
+        text("Score:" + score,10,40);
+        fill("rgba(209, 221, 206, 0.99)");
+        text("Missed:" + missed,10,100);
 
 
-    if (frameCount % 120 == 0){
-        spawnsFruit();                                              // this is where the framecount is
-    }
-    if (mouse.pressing()){
-        trail = new Sprite(mouse.x, mouse.y , 7);
-        trail.collider = 'none';
-        trail.color = "blue";
-        trail.life = 10;
-        sliceFruit();
-    }
-    missedFruit();
+        if (frameCount % 120 == 0){
+            spawnsFruit();                                              // this is where the framecount is
+        }
+        if (mouse.pressing()){
+            trail = new Sprite(mouse.x, mouse.y , 7);
+            trail.collider = 'none';
+            trail.color = "blue";
+            trail.life = 10;
+            sliceFruit();
+        }
+        missedFruit();
         return;
     }else if (gamestate === "gameOver"){
         return;
