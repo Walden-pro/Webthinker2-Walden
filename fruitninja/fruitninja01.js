e1612984354bf923876e121bd1c9c6260b0c894d
Walden-pro <waldenchho@gmail.com>
2026-09-16 08:11:53 +0000

    Sep 16, 2026, 8:11 AM

--- fruitninja/fruitninja01.js
+++ fruitninja/fruitninja01.js
@@ -186,7 +186,8 @@ function missedFruit(){
     for (let fruit of fruitGroup){
         //check if fruit fell below canvas
         if (fruit.y > height + 50){
-            fruit.remove()
+            fruit.remove();
+            missed += 1;
         }
     }
 }
