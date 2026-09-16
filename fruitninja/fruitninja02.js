7b3f63e1861d86f6a6e583d5b9edaade61441e1e
Walden-pro <waldenchho@gmail.com>
2026-09-16 09:06:48 +0000

    Sep 16, 2026, 9:06 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -152,9 +152,9 @@ function preload(){
         scaleMod:2.5,
     }
     let red_onion = {
-        whole : loadImage("assets/onion2.png"),
-        half1: loadImage("assets/onion3.png"),
-        half2: loadImage("assets/onion3.png"),
+        whole : loadImage("assets/red_onion2.png"),
+        half1: loadImage("assets/red_onion3.png"),
+        half2: loadImage("assets/red_onion3.png"),
         scaleMod:2.5,
     }
     //added into fruits
