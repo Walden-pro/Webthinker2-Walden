8686c0fe1e63ec47efc437211948fdd155b40c79
Walden-pro <waldenchho@gmail.com>
2026-09-23 07:52:00 +0000

    Sep 23, 2026, 7:52 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -220,7 +220,7 @@ function draw(){
 
         missedFruit();
         //win or lose condition
-        if (score === 1){
+        if (score === 100){
             gameState = "gameOver";
         }
 
