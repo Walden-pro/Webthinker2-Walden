65be5432810f2aaa11dc82936ab5edfe9482f041
Walden-pro <waldenchho@gmail.com>
2026-09-23 07:53:39 +0000

    Sep 23, 2026, 7:53 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -237,7 +237,7 @@ function draw(){
         fill("rgba(240, 24, 24, 0.99)");
         textSize(100);
         textAlign(CENTER, CENTER);
-        text("Fruit Ninja", width/2, height/2);
+        text("", width/2, height/2);
         return;
     }
 
