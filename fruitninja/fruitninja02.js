fcd17821b791695385fcab6c167c28bf3839a6e9
Walden-pro <waldenchho@gmail.com>
2026-09-23 08:44:22 +0000

    Sep 23, 2026, 8:44 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -234,7 +234,7 @@ function draw(){
         fill("rgba(209, 221, 206, 0.99)");
         text("Missed:" + missed,10,100);
 
-        if (frameCount % 10 == 0){
+        if (frameCount % 20 == 0){
             spawnsFruit();                                              // this is where the framecount is
         }
         if (mouse.pressing()){
