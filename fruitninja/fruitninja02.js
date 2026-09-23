adc1d748aa2a40b7ced6a802a3158d2e411bbb62
Walden-pro <waldenchho@gmail.com>
2026-09-23 08:33:33 +0000

    Sep 23, 2026, 8:33 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -253,7 +253,7 @@ function draw(){
         strokeWeight(3)
         stroke("rgba(10, 13, 9, 0.99)");
         fill("rgba(240, 214, 21, 0.99)");
-        text("Time: " + gameTimer, width / 2, 60);
+        text("Time: " + gameDuration - gameTimer , width / 2, 60);
 
         //win or lose condition
         if (score === 50){
