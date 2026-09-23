d1d7310a61bcb9d6a9c26a7999266c6a102a0cbb
Walden-pro <waldenchho@gmail.com>
2026-09-23 08:10:12 +0000

    Sep 23, 2026, 8:10 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -8,7 +8,8 @@ let score = 0;
 let missed = 0;
 let gameState = 'start';//start,playing , gameover
 let gameStartTime = 0;
-let gameT
+let gameTimer = 0;
+let gameDuration;
 
 function preload(){
     background = loadImage("assets/dojobackground.png")
