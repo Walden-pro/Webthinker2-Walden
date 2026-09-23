8356ada80d710358e0471a4602757b45b1a64d3c
Walden-pro <waldenchho@gmail.com>
2026-09-23 08:07:55 +0000

    Sep 23, 2026, 8:07 AM

--- fruitninja/fruitninja02.js
+++ fruitninja/fruitninja02.js
@@ -165,8 +165,8 @@ function preload(){
     }
     let mangogreen = {
         whole : loadImage("assets/mangogreen1.png"),
-        half1: loadImage("assets/mangogreen2.png"),
-        half2: loadImage("assets/mangogreen2.png"),
+        half1: loadImage("assets/mangogreen3.png"),
+        half2: loadImage("assets/mangogreen3.png"),
         scaleMod:2.5,
     }
     //added into fruits
